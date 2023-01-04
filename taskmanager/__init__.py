b3938ac0a0d07ac91f945a78ba8ad4d9b8c15827
Anna Gabain <anna.gabain@outlook.com>
2023-01-04 16:30:17 +0000

    Template Inheritance

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
 
 
 app = Flask(__name__)
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -20,8 +20,9 @@ class Task(db.Model):
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey(
-        "category.id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(
+        db.Integer, db.ForeignKey(
+            "category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
