9dd159ecee8f41a66bfae363d0851d370fd1d54b
Anna Gabain <anna.gabain@outlook.com>
2023-01-06 21:06:59 +0000

    Adding tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -40,3 +40,21 @@ def delete_category(category_id):
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
+
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task = Task(
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
+            is_urgent=bool(True if request.form.get("is_urgent") else False),
+            due_date=request.form.get("due_date"),
+            category_id=request.form.get("category_id")
+        )
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_task.html", categories=categories)
