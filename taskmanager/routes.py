831031581c07802519240beda44cb2dbd7a00f8f
Anna Gabain <anna.gabain@outlook.com>
2023-01-08 19:27:39 +0000

    Deleting Tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -54,7 +54,6 @@ def add_task():
             due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
         )
-        category = Category(category_name=request.form.get("category_name"))
         db.session.add(task)
         db.session.commit()
         return redirect(url_for("home"))
@@ -73,3 +72,11 @@ def edit_task(task_id):
         task.category_id = request.form.get("category_id")
         db.session.commit()
     return render_template("edit_task.html", task=task, categories=categories)
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
